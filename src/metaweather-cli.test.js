81d87ac2d3fdddd14671bdfdfa8ee1c12e37a98d
Nicholas Eduardo <nicholasess@gmail.com>
2017-08-05 16:41:04 -0300

    test update

--- src/metaweather-cli.test.js
+++ src/metaweather-cli.test.js
@@ -1,4 +1,6 @@
 const metaweather = require('./metaweather')
+const expected = [{"latt_long": "-23.562880,-46.654659", "location_type": "City", "title": "São Paulo", "woeid": 455827}]
+
 
 describe('Test of Metaweather', () => {
 	test('Set city in url', () => {
@@ -10,8 +12,17 @@ describe('Test of Metaweather', () => {
 		expect(metaweather.hasArgs('São Paulo')).toBe(true)
 	})
 
-	test('No has city in arg at command-cli', () => {
+	test('No has city in args at command-cli', () => {
 		expect(metaweather.hasArgs(null)).toBe(false)
+	})
+
+	test('Test fetchApi', () => {
+		metaweather
+		.fetchApi("https://www.metaweather.com/api/location/search/?query=São Paulo")
+		.then((data) => {
+			expect(data).toEqual(expect.arrayContaining(expected));
+		})
+		
 	})
 })
 
